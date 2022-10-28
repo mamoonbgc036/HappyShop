d6451db169ec035faa4751e842b75b3257ddf880
mamoonbgc036 <mamoonbgc0607@gmail.com>
2022-10-29 00:50:25 +0600

    testing

--- functions.php
+++ functions.php
@@ -247,8 +247,4 @@ add_action( 'woocommerce_before_shop_loop_item_title', 'simpleshop_before_shop_l
 add_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 11 );
 add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_price' );
 
-function simpleshop_after_shop_loop_item_title() {
-	echo '<a href="#" class="wish-list"><i class="fa fa-heart-o"></i></a></div>';
-}
 
-add_action( 'woocommerce_after_shop_loop_item_title', 'simpleshop_after_shop_loop_item_title' );
